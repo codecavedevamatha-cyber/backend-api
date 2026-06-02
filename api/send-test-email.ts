8c535d96d4892c8012d800c07ebb2e60409e36ed
Muhammed Irshad <muhammed irshadismail0722@gmail.com>
2026-06-02 20:35:34 +0530

    Add email test

--- api/send-test-email.ts
+++ api/send-test-email.ts
@@ -6,7 +6,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
     const data = await resend.emails.send({
-      from: "website@devamathacollege.ac.in",
+      from: "dmc@devamathacollege.ac.in",
       to: "dmc@devamathacollege.ac.in",
       subject: "Test Email from Vercel",
       html: "<h1>Email system is working!</h1>",
